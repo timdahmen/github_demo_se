cda58589c8b9d89098600921e74567a294269e45
Oskar MBA1 <orakse@gmail.com>
2024-12-03 17:14:41 +0100

    really not good

--- src/de/hsaalen/SnakeTest.java
+++ src/de/hsaalen/SnakeTest.java
@@ -16,7 +16,19 @@ public class SnakeTest
 		coordinate= snake.position(0);
 		assertEquals( coordinate.x, 5 );
 		assertEquals( coordinate.y, 5 );
-		
+		// this does not belong here
+
+export interface TreeItem {
+  id: string;
+  title: string;
+  type: 'vision' | 'goal';
+  parentId: string | null;
+  createdAt: string;
+  zones: {
+    dialog: string;
+    plan: string;
+  };
+}
 		coordinate = snake.position(2);
 		assertEquals( coordinate.x, 3 );
 		assertEquals( coordinate.y, 5 );
